e04e37a1ace099663e315196786bd68c773df4a8
pbca26 <pbca26@gmail.com>
2021-06-16 18:20:49 +0300

    add constants

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+const explorerApiUrl = 'https://explorer.komodoplatform.com:10000/tokensv2/api/';
+const coin = 'TOKENSV2';
+const explorerUrl = 'http://explorer.komodoplatform.com:20000/tokens';
+
+module.exports = {
+  explorerUrl,
+  explorerApiUrl,
+  coin,
+};
