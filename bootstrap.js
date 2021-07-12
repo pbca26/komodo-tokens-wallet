1db1c9e930bb3ad7388543a99ec1b023ca2d1f7f
pbca26 <pbca26@gmail.com>
2021-07-12 15:23:45 +0300

    Updates

--- bootstrap.js
+++ bootstrap.js
@@ -92,7 +92,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b600fe93efef81bb0e43";
+/******/ 	var hotCurrentHash = "eaf245a0e8df3a2fe9a4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
