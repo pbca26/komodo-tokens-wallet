79f4cb739e3a48d22f798c384f6d19b889eca444
pbca26 <pbca26@gmail.com>
2021-12-09 18:57:27 +0300

    update bitgo lib build scripts

--- build-bitgo-lib.sh
+++ build-bitgo-lib.sh
@@ -2,7 +2,7 @@ echo "building bitgo-utxo-lib"
 rm -rf bitgo-utxo-lib
 if test "$1" = 'v2'; then
   echo "bitgo-utxo-lib v2"
-  git clone https://github.com/pbca26/bitgo-utxo-lib -b dimxy-master-v2
+  git clone https://github.com/pbca26/bitgo-utxo-lib -b dimxy-tokel
   node build-bitgo-lib-prep.js v2
 else
   echo "bitgo-utxo-lib v1"
