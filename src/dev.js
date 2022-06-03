593289cf6c9be70b6150987601f1c187a32a5242
pbca26 <pbca26@gmail.com>
2022-06-03 22:32:11 +0300

    add missing bits to properly run tests

---
+++ src/dev.js
@@ -0,0 +1,44 @@
+const dev = {
+  chain: 'TKLTEST',
+  create: {
+    name: 'Test',
+    description: '123',
+    supply: 10,
+    nft: '',
+  },
+  send: {
+    token: {
+      balance: 1,
+      tokenId: '7e2b623cb57b44b8dc5d3a3cc36c20d08f66023a85fa2a4490a8fb8783d38347',
+      name: 'NFTJS',
+    },
+    pubkey: '03256ba44eeb188404b94ae8ed64f1fe6ad89580375830845361e365598efa3ff3',
+    amount: 1,
+  },
+  sell: {
+    token: {
+      balance: 1,
+      tokenId: '7e2b623cb57b44b8dc5d3a3cc36c20d08f66023a85fa2a4490a8fb8783d38347',
+      name: 'NFTJS',
+    },
+    pubkey: '03256ba44eeb188404b94ae8ed64f1fe6ad89580375830845361e365598efa3ff3',
+    amount: 1,
+    price: 0.00001,
+  },
+  buy: {
+    token: {
+      balance: 1,
+      tokenId: '37585125416dfddcb8c8a67a348eaf5d50637f2ce34d47d62f33f14add7c0e97',
+      name: 'Dr Dead',
+    },
+    pubkey: '03256ba44eeb188404b94ae8ed64f1fe6ad89580375830845361e365598efa3ff3',
+    amount: 1,
+    price: 0.00001,
+  },
+  fill: {
+    amountAsk: 1,
+    amountBid: 1,
+  },
+};
+
+export default dev;
