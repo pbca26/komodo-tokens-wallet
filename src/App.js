a391ec1742cc7a5897801f6a53ccb98888ca87f8
pbca26 <pbca26@gmail.com>
2023-01-29 15:32:59 +0300

    rewrite main app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {hot} from 'react-hot-loader';
 import {version} from '../package.json';
 import Login from './Login';
 import Dashboard from './Dashboard';
 import Marketplace from './Marketplace';
-import './debug-util';
 import writeLog from './log';
 import './app.scss';
 import './logo.scss';
@@ -12,85 +11,85 @@ import './input.scss';
 import './order-info.scss';
 import './transactions.scss';
 import './balance.scss';
+import './dashboard.scss';
 import './marketplace.scss';
 
-class App extends React.Component {
-  state = this.initialState;
+const App = () => {
+  const [wif, setWif] = useState('');
+  const [address, setAddress] = useState({
+    normal: '',
+    cc: '',
+  });
+  const [chain, setChain] = useState();
+  const [appVersion, setAppVersion] = useState();
+  const [walletView, setWalletView] = useState(true);
 
-  get initialState() {
-    this.setKey = this.setKey.bind(this);
-    this.resetApp = this.resetApp.bind(this);
-    this.setActiveView = this.setActiveView.bind(this);
-
-    return {
-      wif: '',
-      address: {
-        normal: '',
-        cc: '',
-      },
-      chain: null,
-      appVersion: null,
-      walletView: true,
-    };
-  }
-
-  componentWillMount() {
+  useEffect(() => {
     document.title = `Komodo Tokens Wallet (v${version})`;
-    this.setState({
-      appVersion: version,
+  }, []);
+
+  const resetAppState = () => {
+    setWif('');
+    setAddress({
+      normal: '',
+      cc: '',
     });
-  }
+    setChain();
+    setAppVersion();
+    setWalletView(true);
+  };
 
-  resetApp() {
-    this.setState(this.initialState);
-  }
+  const setActiveViewHandler = (e, activeView) => {
+    console.warn('activeView', activeView)
+    setWalletView(activeView ? true : false);
+  };
 
-  setKey({wif, address, chain}) {
-    this.setState({
-      wif,
+  const setKeyHandler = ({wif, address, chain}) => {
+    setWif(wif);
+    setAddress(address);
+    setChain(chain);
+  };
+
+  useEffect(() => {
+    writeLog('app state', {
       address,
-      chain,
+      walletView,
+      chain
     });
+  });
 
-    setTimeout(() => {
-      writeLog('app this.state', this.state);
-    }, 100);
-  }
-
-  setActiveView() {
-    this.setState({
-      walletView: !this.state.walletView,
-    });
-  }
-
-  render() {
-    return(
-      <React.Fragment>
-        {this.state.appVersion &&
-          <div className="app-version">v{this.state.appVersion}</div>
-        }
-        {!this.state.wif &&
-          <Login setKey={this.setKey} />
-        }
-        {this.state.wif &&
-          <React.Fragment>
-            {this.state.walletView &&
-              <Dashboard
-                resetApp={this.resetApp}
-                setActiveView={this.setActiveView}
-                {...this.state} />
-            }
-            {!this.state.walletView &&
-              <Marketplace
-                resetApp={this.resetApp}
-                setActiveView={this.setActiveView}
-                {...this.state} />
-            }
-          </React.Fragment>
-        }
-      </React.Fragment>
-    );
-  }
-}
+  return(
+    <React.Fragment>
+      {appVersion &&
+        <div className="app-version">v{appVersion}</div>
+      }
+      {!wif &&
+        <Login setKey={setKeyHandler} />
+      }
+      {wif &&
+        <React.Fragment>
+          {walletView &&
+            <Dashboard
+              resetApp={resetAppState}
+              setActiveView={setActiveViewHandler}
+              walletView={walletView}
+              address={address}
+              chain={chain}
+              wif={wif} />
+          }
+          {!walletView &&
+            <Marketplace
+              resetApp={resetAppState}
+              setActiveView={setActiveViewHandler}
+              walletView={walletView}
+              address={address}
+              chain={chain}
+              wif={wif} />
+          }
+        </React.Fragment>
+      }
+    </React.Fragment>
+  );
+};
 
 export default hot(module)(App);
