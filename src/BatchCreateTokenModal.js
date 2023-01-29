2327b777f4963f38d9c457c5241198faf4e8d867
pbca26 <pbca26@gmail.com>
2023-01-29 14:43:43 +0300

    rewrite batch create token modal

--- src/BatchCreateTokenModal.js
+++ src/BatchCreateTokenModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Modal from './Modal';
 import TokensLib from './cclib-import';
 import Blockchain from './blockchain';
@@ -6,103 +6,95 @@ import {chains} from './constants';
 import {utxoSelectNormal} from './utxo-select';
 
 const jsonFormatExample = [{
-  "name": "token1",
-  "supply": 1,
-  "description": "Some description",
-  "nft": "{\"url\": \"someurl\", \"id\": 1, \"royalty\": 10, \"arbitrary\": {\"some key\":\"some prop\"}}"
+  'name': 'token1',
+  'supply': 1,
+  'description': 'Some description',
+  'nft': '{\"url\": \"someurl\", \"id\": 1, \"royalty\": 10, \"arbitrary\": {\"some key\":\"some prop\"}}'
 }];
 
 let checkTxConfsTimer;
 
-class BatchCreateTokenModal extends React.Component {
-  state = this.initialState;
-  
-  get initialState() {
-    this.updateInput = this.updateInput.bind(this);
-    this.getMaxSupply = this.getMaxSupply.bind(this);
+const BatchCreateTokenModal = props => {
+  const initialState = {
+    isClosed: true,
+    batchJson: null,
+    currentTokenNumber: 0,
+    currentTokenName: null,
+    totalTokens: 0,
+    success: null,
+    error: null,
+    inProgress: -1,
+  };
+  const [state, setState] = useState(initialState);
 
-    return {
-      isClosed: true,
-      batchJson: null,
-      currentTokenNumber: 0,
-      currentTokenName: null,
-      totalTokens: 0,
-      success: null,
-      error: null,
-      inProgress: -1,
-    };
-  }
-
-  checkTxConfs = async txid => {
-    const self = this;
-
+  const checkTxConfs = async txid => {
     try {
       const txData = await Blockchain.getTransaction(txid);
       console.warn('txData', txData);
 
       if (txData && txData.confirmations && Number(txData.confirmations) >= 1) {
         console.warn(txid, 'is confirmed');
-        this.props.syncData();
+        props.syncData();
         if (checkTxConfsTimer) clearTimeout(checkTxConfsTimer);
         const txData1 = await Blockchain.getTransaction(txid);
-        this.createNewTokenBatch(true);
+        createNewTokenBatch(true);
       } else {
         checkTxConfsTimer = setTimeout(() => {
-          self.checkTxConfs(txid);
+          checkTxConfs(txid);
         }, 5000);
       }
     } catch (e) {
       console.warn('error fetching confs data, retry', e);
       checkTxConfsTimer = setTimeout(() => {
-        self.checkTxConfs(txid);
+        checkTxConfs(txid);
       }, 5000);
     }
   };
 
-  updateInput(e) {
-    this.setState({
+  const updateInput = e => {
+    setState(prevState => ({
+      ...prevState,
       [e.target.name]: e.target.value,
       error: null,
       success: null,
-    });
-
-    if (window.DEBUG) {
-      setTimeout(() => {
-        console.warn('login this.state', this.state);
-      }, 100);
-    }
+    }));
   }
 
-  close() {
-    this.setState({
-      ...this.initialState,
+  useEffect(() => {
+    writeLog('sell token modal state', state);
+  });
+
+  const close = () => {
+    setState({
+      ...initialState,
       isClosed: true,
     });
   }
 
-  open() {
-    if (this.props.normalUtxos.length)
-      this.setState({
-        ...this.initialState,
+  const open = () => {
+    if (props.normalUtxos.length)
+      setState({
+        ...initialState,
         isClosed: false,
       });
   }
 
-  createNewTokenBatch = async (isAuto) => {
-    const tokens = JSON.parse(this.state.batchJson);
-    const i = this.state.currentTokenNumber;
+  const createNewTokenBatch = async (isAuto) => {
+    const tokens = JSON.parse(state.batchJson);
+    const i = state.currentTokenNumber;
     let rawtx;
     //console.log(tokens);
     //console.log(typeof tokens);
 
     if (!isAuto) {
-      this.setState({
+      setState(prevState => ({
+        ...prevState,
         success: null,
         error: null,
         currentTokenName: null,
         currentTokenNumber: 0,
         totalTokens: 0,
-      });
+      }));
     }
 
     const formatNftData = (data) => {
@@ -129,25 +121,25 @@ class BatchCreateTokenModal extends React.Component {
       try {
         let inputsData;
 
-        console.warn('create token modal txBuilderApi', chains[this.props.chain].txBuilderApi);
+        console.warn('create token modal txBuilderApi', chains[props.chain].txBuilderApi);
         
-        if (chains[this.props.chain].txBuilderApi === 'utxoSelect') {
-          inputsData = await utxoSelectNormal(this.props.address.normal, Number(tokens[i].supply) + 20000, true, chains[this.props.chain].ccLibVersion);
+        if (chains[props.chain].txBuilderApi === 'utxoSelect') {
+          inputsData = await utxoSelectNormal(props.address.normal, Number(tokens[i].supply) + 20000, true, chains[props.chain].ccLibVersion);
         } else {
-          inputsData = chains[this.props.chain].txBuilderApi === 'default' ? await TokensLib[chains[this.props.chain].ccLibVersion === 1 ? 'V1' : 'V2'].createTxAndAddNormalInputs(Number(tokens[i].supply) + 10000 + 10000, this.props.address.pubkey) : await Blockchain.createCCTx(Number(tokens[i].supply) + 10000 + 10000, this.props.address.pubkey);
+          inputsData = chains[props.chain].txBuilderApi === 'default' ? await TokensLib[chains[props.chain].ccLibVersion === 1 ? 'V1' : 'V2'].createTxAndAddNormalInputs(Number(tokens[i].supply) + 10000 + 10000, props.address.pubkey) : await Blockchain.createCCTx(Number(tokens[i].supply) + 10000 + 10000, props.address.pubkey);
         }
         
         if (window.DEBUG) {
           console.warn('create tx modal inputsData', inputsData);
         }
 
-        const createTxFunc = chains[this.props.chain].ccLibVersion === 1 ? TokensLib.V1.createTokenTx : tokens[i].nft.indexOf('{') > -1 ? TokensLib.V2.createTokenTxTokel : TokensLib.V2.createTokenTx;
+        const createTxFunc = chains[props.chain].ccLibVersion === 1 ? TokensLib.V1.createTokenTx : tokens[i].nft.indexOf('{') > -1 ? TokensLib.V2.createTokenTxTokel : TokensLib.V2.createTokenTx;
         console.warn('createTxFunc', createTxFunc)
         const createTxPayload = tokens[i].nft.length > 0 ? {
           name: tokens[i].name, 
           description: tokens[i].description,
           supply: Number(tokens[i].supply),
-          nft: chains[this.props.chain].ccLibVersion === 1 ? (tokens[i].nft.indexOf('{') > -1 ? 'f7' : '00') : tokens[i].nft.indexOf('{') > -1 ? formatNftData(tokens[i].nft) : '00' + Buffer.from(tokens[i].nft).toString('hex'),
+          nft: chains[props.chain].ccLibVersion === 1 ? (tokens[i].nft.indexOf('{') > -1 ? 'f7' : '00') : tokens[i].nft.indexOf('{') > -1 ? formatNftData(tokens[i].nft) : '00' + Buffer.from(tokens[i].nft).toString('hex'),
         } : {
           name: tokens[i].name, 
           description: tokens[i].description,
@@ -157,7 +149,7 @@ class BatchCreateTokenModal extends React.Component {
         rawtx = await createTxFunc(
           inputsData,
           createTxPayload,
-          this.props.wif
+          props.wif
         );
 
         if (window.DEBUG) {
@@ -167,53 +159,59 @@ class BatchCreateTokenModal extends React.Component {
         if (rawtx && rawtx.substr(0, 2) === '04') {
           //const txid = 'eea164a024dd72fa41de54f400e090cd93e6327005e8148720b674f5e43dd044';
           const {txid} = await Blockchain.broadcast(rawtx);
-          /*this.setState({
+          /*setState(prevState => ({
+            ...prevState,
             success: '123',
             error: null,
             currentTokenName: tokens[i].name,
-            currentTokenNumber: this.state.currentTokenNumber + 1,
+            currentTokenNumber: state.currentTokenNumber + 1,
           });*/
 
           if (!txid || txid.length !== 64) {
-            this.setState({
+            setState(prevState => ({
+              ...prevState,
               success: null,
               error: 'Unable to broadcast transaction!',
-            });
+            }));
           } else {
-            this.setState({
+            setState(prevState => ({
+              ...prevState,
               success: txid,
               error: null,
               currentTokenName: tokens[i].name,
-              currentTokenNumber: this.state.currentTokenNumber + 1,
+              currentTokenNumber: state.currentTokenNumber + 1,
               totalTokens: tokens.length,
               inProgress: true,
-            });
+            }));
 
-            this.checkTxConfs(txid);
+            checkTxConfs(txid);
           }
         } else {
-          this.setState({
+          setState(prevState => ({
+            ...prevState,
             success: null,
             error: 'Unable to create transaction!',
-          });
+          }));
         }
       } catch (e) {
-        this.setState({
+        setState(prevState => ({
+          ...prevState,
           success: null,
           error: e.message,
-        });
+        }));
       }
     } else {
-      this.setState({
+      setState(prevState => ({
+        ...prevState,
         success: true,
         inProgress: false,
         batchJson: null,
-      });
+      }));
     }
   }
 
-  getMaxSupply() {
-    const normalUtxos = this.props.normalUtxos;
+  const getMaxSupply = () => {
+    const normalUtxos = props.normalUtxos;
     let maxSupply = -20000; // maker + tx fee
 
     for (let i = 0; i < normalUtxos.length; i++) {
@@ -223,18 +221,18 @@ class BatchCreateTokenModal extends React.Component {
     return maxSupply < 0 ? 0 : maxSupply;
   };
 
-  render() {
+  const render = () => {
     return (
       <React.Fragment>
         <div
-          className={`token-tile create-new-trigger${this.getMaxSupply() === 0 ? ' disabled' : ''}`}
-          onClick={() => this.open()}>
+          className={`token-tile create-new-trigger${getMaxSupply() === 0 ? ' disabled' : ''}`}
+          onClick={() => open()}>
           <i className="fa fa-plus"></i>
           Batch Create
         </div>
         <Modal
-          show={this.state.isClosed === false}
-          handleClose={() => this.close()}
+          show={state.isClosed === false}
+          handleClose={() => close()}
           isCloseable={true}
           className="Modal-create-token">
           <div className="create-token-form">
@@ -246,9 +244,9 @@ class BatchCreateTokenModal extends React.Component {
                 cols="33"
                 name="batchJson"
                 placeholder="Tokens list in JSON format"
-                value={this.state.batchJson}
-                onChange={this.updateInput}
-                disabled={this.state.inProgress === true}>
+                value={state.batchJson}
+                onChange={updateInput}
+                disabled={state.inProgress === true}>
               </textarea>
               <p>
                 JSON format example:
@@ -258,37 +256,37 @@ class BatchCreateTokenModal extends React.Component {
               </p>
               <button
                 type="button"
-                onClick={this.createNewTokenBatch}
+                onClick={createNewTokenBatch}
                 disabled={
-                  !this.state.batchJson ||
-                  this.getMaxSupply() === 0 ||
-                  this.state.inProgress === true
+                  !state.batchJson ||
+                  getMaxSupply() === 0 ||
+                  state.inProgress === true
                 }>Create</button>
-              {this.state.success &&
-               this.state.inProgress === true &&
+              {state.success &&
+               state.inProgress === true &&
                 <div className="success">
-                  Token <strong style={{'display': 'inline-block'}}>{this.state.currentTokenName}</strong> ({this.state.currentTokenNumber} of {this.state.totalTokens}) created!
+                  Token <strong style={{'display': 'inline-block'}}>{state.currentTokenName}</strong> ({state.currentTokenNumber} of {state.totalTokens}) created!
                   <div className="txid-label" style={{'paddingBottom': '20px'}}>
-                    <strong>Transaction ID:</strong> {this.state.success}
+                    <strong>Transaction ID:</strong> {state.success}
                   </div>
                   <a
-                    href={`${chains[this.props.chain].explorerUrl}/${this.state.success}/transactions/${this.props.chain}`}
+                    href={`${chains[props.chain].explorerUrl}/${state.success}/transactions/${props.chain}`}
                     target="_blank">Open on explorer</a>
                   <p>Awaiting confirmations...</p>
                 </div>
               }
-              {this.state.success &&
-               this.state.inProgress === false &&
+              {state.success &&
+               state.inProgress === false &&
                 <div className="success">
                   All tokens are created!
-                  <p>{this.state.totalTokens} in total</p>
+                  <p>{state.totalTokens} in total</p>
                 </div>
               }
-              {this.state.error &&
+              {state.error &&
                 <div className="error">
                   <div>
                     <strong>Error!</strong>
-                    <div>{this.state.error}</div>
+                    <div>{state.error}</div>
                   </div>
                 </div>
               }
@@ -298,6 +296,8 @@ class BatchCreateTokenModal extends React.Component {
       </React.Fragment>
     );
   }
+
+  return render();
 }
 
 export default BatchCreateTokenModal;
